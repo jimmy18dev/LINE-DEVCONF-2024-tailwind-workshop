807e3136e4a878abc6b0554a0061cc1925509133
Puwadon Sricharoen <mrjimmy18@gmail.com>
2024-10-29 21:11:27 +0700

    fix: Disable the first video card item.

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@ import HeaderBar from './components/HeaderBar.vue'
   <div class="flex flex-col">
     <!-- <HeaderBar /> -->
     <template v-for="index in 1" :key="index">
-      <VideoCard :postId="1" />
+      <!-- <VideoCard :postId="1" /> -->
       <!-- <VideoCard :postId="2" /> -->
       <!-- <VideoCard :postId="3" /> -->
       <!-- <VideoCard :postId="4" /> -->
